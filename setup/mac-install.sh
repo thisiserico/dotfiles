c306c76db83bbb0f83ee6ee7489206aab94bfabc
Eric Lopez <hello@ericlopez.me>
2017-12-04 22:03:58 +0100

    install the tree program in osx

--- setup/mac-install.sh
+++ setup/mac-install.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install utilities
-brew install wget git zsh zsh-completions wget tmux tmuxinator-completion
+brew install wget git zsh zsh-completions wget tmux tmuxinator-completion tree
 sudo gem install tmuxinator
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
